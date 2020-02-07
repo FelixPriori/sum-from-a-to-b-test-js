f21442de0de634fea99dadb845a2bdec06b0bdca
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-07 00:44:16 +0000

    Solved

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 
+// NON RECURSIVE
+// function sum(fromN, toN) {
+//   // Sum all the values from fromN up to toN
+//   let count = fromN;
+//   for (let i = fromN; i < toN; i++) {
+//     count += i + 1;
+//   }
+//   return count;
+// }
+// sum(3, 7);
+
+//RECURSIVE ??????
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+
+  if (fromN === toN) {
+    return fromN;
+  }
+  // console.log(fromN);
+  return fromN + sum(fromN + 1, toN);
 }
+
+// sum(3, 7);
 
 module.exports = sum;
